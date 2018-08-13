03d8219af9b07ad4c337be89addaf71a819e88bc
DenVilden <oovilden@gmail.com>
2018-08-13 23:33:44 +0500

    Мелкие правки

--- js/rules.js
+++ js/rules.js
@@ -45,7 +45,7 @@ backButton.addEventListener(`click`, () => selectSlide(greetingScreen));
 
 // Отключает кнопку если input пустой
 rulesInput.addEventListener(`input`, (evt) => {
-  return (rulesButton.disabled = evt.target.value.length === 0);
+  rulesButton.disabled = evt.target.value.length === 0;
 });
 
 export default rulesScreen;
