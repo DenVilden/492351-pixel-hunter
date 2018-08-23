6d0c47dc49801a10f721855cfd0238aff711b9ad
DenVilden <oovilden@gmail.com>
2018-08-23 14:10:15 +0500

    Убирает лишние переменные

--- js/data/game-data.js
+++ js/data/game-data.js
@@ -5,6 +5,14 @@ export const data = {
     time: 30
   }),
 
+  SCORE: {
+    RIGHT_ANSWER: 100,
+    BONUS_ANSWER: 50,
+    ONE_LIFE_SCORE: 50,
+    SLOW_ANSWER: 20, // в секундах
+    QUICK_ANSWER: 10
+  },
+
   populateAnswers(completed, timeSpent, length) {
     const answers = [];
 
@@ -17,30 +25,25 @@ export const data = {
     return answers;
   },
   calculateScore(answers, lives) {
-    const RIGHT_ANSWER = 100;
-    const QUICK_ANSWER = 50;
-    const SLOW_ANSWER = 50;
-    const ONE_LIFE_SCORE = 50;
-
-    let score = 0;
-
     if (answers.length < 10) {
       return -1;
     }
-
-    answers.forEach((answer) => {
-      if (answer.completed) {
-        score += RIGHT_ANSWER;
-      }
-      if (answer.completed && answer.timeSpent < 10) {
-        score += QUICK_ANSWER;
-      }
-      if (answer.completed && answer.timeSpent > 20) {
-        score -= SLOW_ANSWER;
-      }
-    });
     // Возвращает очки + очки за жизни
-    return score + this.setLives(lives) * ONE_LIFE_SCORE;
+    return (
+      answers.reduce((score, answer) => {
+        if (answer.completed) {
+          score += this.SCORE.RIGHT_ANSWER;
+        }
+        if (answer.completed && answer.timeSpent < this.SCORE.QUICK_ANSWER) {
+          score += this.SCORE.BONUS_ANSWER;
+        }
+        if (answer.completed && answer.timeSpent > this.SCORE.SLOW_ANSWER) {
+          score -= this.SCORE.BONUS_ANSWER;
+        }
+        return score;
+      }, 0) +
+      this.setLives(lives) * this.SCORE.ONE_LIFE_SCORE
+    );
   },
   changeLevel(level) {
     if (typeof level !== `number`) {
--- js/data/game-data.test.js
+++ js/data/game-data.test.js
@@ -4,36 +4,30 @@ import {data} from "./game-data";
 describe(`Calculate score`, () => {
   it(`should return -1 if array contains less than 10 answers`, () => {
     assert.equal(
-        -1,
         data.calculateScore(data.populateAnswers(true, 20, 9), 3),
+        -1,
         `game is completed`
     );
   });
   it(`should return 1150 if the game is completed normally with all lives`, () => {
     assert.equal(
+        data.calculateScore(data.populateAnswers(true, 20, 10), 3),
         1150,
-        data.calculateScore(
-            data.populateAnswers(true, 20, 10),
-            3,
-            `something went wrong`
-        )
+        `something went wrong`
     );
   });
   it(`should return 1650 if the game is completed fast with all lives`, () => {
     assert.equal(
+        data.calculateScore(data.populateAnswers(true, 5, 10), 3),
         1650,
-        data.calculateScore(
-            data.populateAnswers(true, 5, 10),
-            3,
-            `something went wrong`
-        )
+        `something went wrong`
     );
   });
 });
 
 describe(`Change level`, () => {
   it(`should update level of the game`, () => {
-    assert.equal(50, data.changeLevel(50), `level hasnt updated`);
+    assert.equal(data.changeLevel(50), 50, `level hasnt updated`);
   });
   it(`should not allow set negative values`, () => {
     assert.throws(
@@ -48,7 +42,7 @@ describe(`Change level`, () => {
 
 describe(`Set time`, () => {
   it(`should set timer`, () => {
-    assert.equal(20, data.setTime(20), `cant set time`);
+    assert.equal(data.setTime(20), 20, `cant set time`);
   });
   it(`should check if time limit is 30 seconds`, () => {
     assert.throws(() => data.setTime(40), `Time limit is 30 seconds`);
@@ -57,7 +51,7 @@ describe(`Set time`, () => {
 
 describe(`Set lives`, () => {
   it(`should set live`, () => {
-    assert.equal(2, data.setLives(2), `cant update lives`);
+    assert.equal(data.setLives(2), 2, `cant update lives`);
   });
   it(`should end the game if all lives spent`, () => {
     assert.throws(() => data.setLives(0), `Game is over, no more lives`);
